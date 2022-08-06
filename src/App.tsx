83d7403edb055567902249168b940a60b69b5217
BigBen3918 <benjamin199551@gmail.com>
2022-08-06 09:40:47 -0400

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.scss";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <img src={logo} className="App-logo" alt="logo" />
+            {/* Variable Example */}
+            <h1>This is a variable example</h1>
+
+            {/* Nested Rules */}
+            <div id="nested">
+                <h3>This is a nested rules example</h3>
+            </div>
+
+            {/* Interpolation Example */}
+            <p className="interpolation">Interpolation Example.</p>
+
+            {/* Function Example */}
+            <div id="function">
+                <h3>This is a function example</h3>
+            </div>
+
+            {/* Mixin Example */}
+            <div id="mixin">
+                <p>This is mixin Example</p>
+            </div>
+        </div>
+    );
 }
 
 export default App;
